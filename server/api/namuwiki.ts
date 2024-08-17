d50bce0faf2fd190c37ee850fc9206d8787007f9
youngkyunjung <younginpiniti@gmail.com>
2024-08-17 14:00:40 +0900

    Referer 헤더 추가

--- server/api/namuwiki.ts
+++ server/api/namuwiki.ts
@@ -13,6 +13,7 @@ export default defineEventHandler(async (event) => {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
           "Content-Type": "text/html",
+          Referer: "https://namu.wiki/",
         },
         responseType: "text",
       });
