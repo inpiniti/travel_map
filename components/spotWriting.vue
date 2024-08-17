4490f1d1e3f6fd12c2346c892dc8a0d7cca48140
youngkyunjung <younginpiniti@gmail.com>
2024-08-18 08:48:54 +0900

    등록시, 평점, 리뷰, 영업시간 등도 포함되도록

--- components/spotWriting.vue
+++ components/spotWriting.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { city } from "../data/city";
 const { setSpot, status, getSpotOfGoogle } = useSpot();
+
 const form = ref({
   spot_name: "",
   description: "",
@@ -8,6 +10,15 @@ const form = ref({
   longitude: 0,
   type: "",
   city: "",
+  rating: 0, // 평점
+  reviews: 0, // 리뷰 수
+  opening: "", // 영업시간
+  veryRelaxed: "", // 매우 여유로운 시간대
+  relaxed: "", // 여유로운 시간대
+  moderate: "", // 보통 시간대
+  somewhatCrowded: "", // 다소 혼잡한 시간대
+  veryCrowded: "", // 매우 혼잡한 시간대
+  detailType: "", // 상세타입
 });
 
 const saveSpot = async () => {
@@ -40,6 +51,16 @@ const getSpot = async () => {
       image: res.image,
       latitude: res.latitude,
       longitude: res.longitude,
+
+      rating: res.rating,
+      reviews: res.reviews,
+      opening: res.opening,
+      veryRelaxed: res.veryRelaxed,
+      relaxed: res.relaxed,
+      moderate: res.moderate,
+      somewhatCrowded: res.somewhatCrowded,
+      veryCrowded: res.veryCrowded,
+      detailType: res.type,
     };
   }
 };
@@ -60,11 +81,38 @@ const getSpot = async () => {
       <div class="grid gap-4 py-4">
         <div class="grid items-center grid-cols-4 gap-4">
           <Label class="text-right"> 장소 유형 </Label>
-          <Input type="text" class="col-span-3" v-model="form.type" readonly />
+          <Select v-model="form.type">
+            <SelectTrigger class="col-span-3">
+              <SelectValue placeholder="Select a city" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem :value="category" v-for="category in CATEGORYS">
+                  {{ category }}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
+
         <div class="grid items-center grid-cols-4 gap-4">
           <Label class="text-right"> 도시 </Label>
-          <Input type="text" class="col-span-3" v-model="form.city" readonly />
+          <Select v-model="form.city">
+            <SelectTrigger class="col-span-3">
+              <SelectValue placeholder="Select a city" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="전체"> 전체 </SelectItem>
+                <template v-for="key in Object.keys(city)">
+                  <SelectLabel>{{ key }}</SelectLabel>
+                  <SelectItem :value="city_name" v-for="city_name in city[key]">
+                    {{ city_name }}
+                  </SelectItem>
+                </template>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <div class="grid items-center grid-cols-4 gap-4">
           <Label class="text-right"> 장소명 </Label>
@@ -74,6 +122,10 @@ const getSpot = async () => {
             v-model="form.spot_name"
           />
           <Button class="col-span-1" @click="getSpot">가져오기</Button>
+        </div>
+        <div class="grid items-center grid-cols-4 gap-4">
+          <Label class="text-right">상세 유형</Label>
+          <Input type="text" class="col-span-3" v-model="form.detailType" />
         </div>
         <div class="grid items-center grid-cols-4 gap-4">
           <Label class="text-right"> 장소 설명 </Label>
@@ -107,6 +159,22 @@ const getSpot = async () => {
             class="col-span-3"
             placeholder="130.401716"
             v-model="form.longitude"
+          />
+        </div>
+        <div class="grid items-center grid-cols-4 gap-4">
+          <Label class="text-right"> 평점 </Label>
+          <Input class="col-span-3" placeholder="4.5" v-model="form.rating" />
+        </div>
+        <div class="grid items-center grid-cols-4 gap-4">
+          <Label class="text-right"> 리뷰수 </Label>
+          <Input class="col-span-3" placeholder="345" v-model="form.reviews" />
+        </div>
+        <div class="grid items-center grid-cols-4 gap-4">
+          <Label class="text-right"> 영업시간 </Label>
+          <Input
+            class="col-span-3"
+            placeholder="오전 9:30~오후 6:15"
+            v-model="form.opening"
           />
         </div>
       </div>
