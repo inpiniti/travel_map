593c44283b7b64ac79ceba7b991e48142fe40d75
inpiniti <inpiniti@gmail.com>
2024-08-28 19:00:05 +0900

    title 및 content 업데이트

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,12 +51,19 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "맞춤형 여행 계획 지도",
       meta: [
         { charset: "utf-8" },
         {
           name: "viewport",
           content:
             "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "맞춤형 여행 계획 지도를 통해 나만의 여행 경로를 짜보고, 다양한 여행지 정보를 확인하세요. 여행 계획부터 실행까지, 모든 과정을 한 곳에서 해결할 수 있습니다.",
         },
       ],
     },
