e06825363ae42c7eeaf531d638c49423e61e7372
youngkyunjung <younginpiniti@gmail.com>
2024-08-29 22:19:46 +0900

    refactor: Update app.vue and tailwind.css for consistent color scheme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,78 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "맞춤형 여행 계획 지도",
-      link: [{ rel: "icon", type: "image/png", href: "/fried-potatoes.png" }],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "57x57",
+          href: "/apple-icon-57x57.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "60x60",
+          href: "/apple-icon-60x60.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "72x72",
+          href: "/apple-icon-72x72.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "76x76",
+          href: "/apple-icon-76x76.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "114x114",
+          href: "/apple-icon-114x114.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "120x120",
+          href: "/apple-icon-120x120.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "144x144",
+          href: "/apple-icon-144x144.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "152x152",
+          href: "/apple-icon-152x152.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-icon-180x180.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-icon-192x192.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "96x96",
+          href: "/favicon-96x96.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/manifest.json" },
+      ],
       meta: [
         { charset: "utf-8" },
         {
@@ -115,6 +186,8 @@ export default defineNuxtConfig({
           content:
             "맞춤형 여행 계획 지도를 통해 나만의 여행 경로를 짜보고, 다양한 여행지 정보를 확인하세요. 여행 계획부터 실행까지, 모든 과정을 한 곳에서 해결할 수 있습니다.",
         },
+        { name: "msapplication-TileColor", content: "#ffffff" },
+        { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
       ],
     },
   },
